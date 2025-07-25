a03ea9db5475740bbf6009058b4b30d9640f3f0e
Minh11042010 <dtm11042010@gmail.com>
2025-07-25 12:05:39 +0700

    commit

---
+++ BaiTap24-7/Nhom4/35.py
@@ -0,0 +1,17 @@
+def check(n):
+    k = 0
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            k += 1
+            if i != n / i:
+                k += 1
+    if k == 2:
+        return 1
+    else:
+        return 0
+
+
+n = int(input())
+for i in range(1, n + 1):
+    if check(i) == 1:
+        print(i, end=" ")
