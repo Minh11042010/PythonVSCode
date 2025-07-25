459a262c8d66824977a66424061aee041dba0be4
Minh11042010 <dtm11042010@gmail.com>
2025-07-25 22:02:01 +0700

    commit

---
+++ BaiTap24-7/Nhom5/41.py
@@ -0,0 +1,11 @@
+diem = float(input())
+if diem >= 9.0:
+    print("Xuat sac")
+elif diem >= 8.0:
+    print("Gioi")
+elif diem >= 6.5:
+    print("Kha")
+elif diem >= 5.0:
+    print("Trung binh")
+else:
+    print("Yeu")
