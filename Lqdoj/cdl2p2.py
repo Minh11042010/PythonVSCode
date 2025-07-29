4ae7687457a6b264940e7e57dcbdd6697bce38fc
Minh11042010 <dtm11042010@gmail.com>
2025-07-29 17:02:20 +0700

    commit

---
+++ Lqdoj/cdl2p2.py
@@ -0,0 +1,9 @@
+x = int(input())
+y = int(input())
+
+if round(x / 10) > y:
+    print("Arabica mat mua.")
+    print("He so gia ban: 3")
+else:
+    print("Arabica duoc mua.")
+    print("He so gia ban: 2")
